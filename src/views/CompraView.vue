9c41ad68951bb78546614f8b1b4e37cc6500cbb6
Carolina <cdamianhdez@gmail.com>
2023-09-20 09:43:04 -0600

    Acordeón

--- src/views/CompraView.vue
+++ src/views/CompraView.vue
@@ -2,19 +2,19 @@
 import { RouterLink, RouterView } from 'vue-router';
 import Progreso from '@/components/Progreso.vue';
 
-myCollapseEl.addEventListener('shown.bs.collapse', event => {
-    document.getElementById("dirección").style.display = "block";
-
-myCollapseEl.addEventListener('shown.bs.collapse', event => {
-    document.getElementById("pago").style.display = "block";
-
-myCollapseEl.addEventListener('shown.bs.collapse', event => {
-    document.getElementById("elaboración").style.display = "block";
-
-myCollapseEl.addEventListener('shown.bs.collapse', event => {
-    document.getElementById("envío").style.display = "block";
-
-</script>
+//myCollapseEl.addEventListener('shown.bs.collapse'), event => {
+//    document.getElementById("dirección").style.display = "block";
+//}
+//myCollapseEl.addEventListener('shown.bs.collapse'), event => {
+//    document.getElementById("pago").style.display = "block";
+//}
+//myCollapseEl.addEventListener('shown.bs.collapse'), event => {
+//    document.getElementById("elaboración").style.display = "block";
+//}
+//myCollapseEl.addEventListener('shown.bs.collapse'), event => {
+//    document.getElementById("envío").style.display = "block";
+//}
+//</script>
 
 <template>
     <main class="main__desk">
@@ -22,12 +22,28 @@ myCollapseEl.addEventListener('shown.bs.collapse', event => {
             <Progreso />
 
         </div>
-
-        <div id="dirección" style="display: none;">Componente de dirección</div>
-        <div id="pago" style="display: none;">Componente de pago</div>
-        <div id="elaboración" style="display: none">Componente de elaboración</div>
-        <div id="envío" style="display: none;">Componente de envío</div>
-
+        <div id="accordion">
+            <div class="card" id="dirección">
+                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                Componente de dirección
+                </button>
+            </div>
+            <div class="card" id="pago">
+                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                Componente de pago
+                </button>
+            </div>
+            <div class="card" id="elaboración">
+                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                Componente de elaboración
+                </button>
+            </div>
+            <div class="card" id="envío">
+                <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                Componente de envío
+                </button>
+            </div>
+        </div>
     </main>
 
 
